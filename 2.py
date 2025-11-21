d4009058a8cb101f2d69267c63712a2e55fce605
DNS <violettarybacenko@gmail.com>
2025-11-21 23:16:14 +1000

    рипит

--- 2.py
+++ 2.py
@@ -1,7 +1,5 @@
-repeat=input("Ну шо, еще партейку?")
-
 attenpt = 1
-while repeat.lower() != 'нет':
-    print('Ну пошло добро по трубам! Трай', attenpt)
+while (repeat := input("Ну шо, еще партейку?")) != 'нет':
+
+    print(f'Да, сам согласился {repeat}, Ну пошло добро по трубам! Трай', attenpt)
     attenpt += 1
-    repeat=input("Ну шо, еще партейку?")
