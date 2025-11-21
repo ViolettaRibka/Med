dac996e14713dcc4bf8f71f386b84dd4e97294b5
DNS <violettarybacenko@gmail.com>
2025-11-21 23:25:26 +1000

    цикл while

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 attenpt = 1
-while (repeat := input("Ну шо, еще партейку?")) != 'нет':
+while (repeat := input("Ну шо, еще партейку, ок да ?")) != 'нет':
 
     print(f'Да, сам согласился {repeat}, Ну пошло добро по трубам! Трай', attenpt)
     attenpt += 1
